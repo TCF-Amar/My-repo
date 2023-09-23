10d57f322e3f9cf6d6a1bee8cd51f425c1e57a79
TCF-Amar <amarjeetmistri41@gmail.com>
2023-09-23 19:53:00 +0530

    Cpp challenge code

--- project.cpp
+++ project.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <string>
+#include <ctime>
 using namespace std;
+int delay(int millisecond)
+{
+    clock_t gole = millisecond + clock();
+    while (gole > clock())
+        ;
+    return 1;
+}
 int main()
 {
-    cout << "Hello World";
-    cout << "My name is amar";
+    string str = "ABCDEEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    string name, res;
+    cout << "Enter word :- ";
+    getline(cin, name);
+
+    for (int i = 0; name[i] >= str.size(); i++)
+    {
+        for (int j = 0; name[i] >= str[j]; j++)
+        {
+            cout << res << str[j] << endl;
+            delay(2 * 100);
+        }
+        res += name[i];
+    }
+
     return 0;
 }
